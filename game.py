c3c7b0d3f4ac28ea19129e30de3999c8bc6e83e4
Elenaz000 <yz760@georgetown.edu>
2022-01-26 17:28:45 -0600

    Update game.py

--- game.py
+++ game.py
@@ -5,3 +5,18 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+#ask for input
+x=input("what is your choice?")
+
+#validate user's input
+print (x)
+
+#computer choice
+
+
+#determine the winner
+
+
+#final result
